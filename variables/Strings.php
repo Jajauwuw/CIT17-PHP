646bc096bd6a9d67a4e63466772b7935d17fd87f
khurt Wigwigan <102198085+Jajauwuw@users.noreply.github.com>
2023-10-03 18:07:25 +0800

    initial commit

---
+++ variables/Strings.php
@@ -0,0 +1,9 @@
+<?php
+
+$string_1 = "This is a string in double quotes";
+$string_2 = "This is a somewhat longer, singly quoted string";
+$string_39 = "This string has thirty-nine characters";
+$string_0 = ""; // a string with zero characters
+
+
+?>
