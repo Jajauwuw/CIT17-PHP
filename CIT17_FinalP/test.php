9566af9111f645ac73f5d2554153afa79c6af52a
khurt Wigwigan <102198085+Jajauwuw@users.noreply.github.com>
2023-12-18 20:45:40 +0800

    first commit

---
+++ CIT17_FinalP/test.php
@@ -0,0 +1,17 @@
+<?php
+
+$name = "Aldrin";
+$age = 20;
+$greetings = print("Hi my name is $name and I am $age years old");
+
+
+print($greetings);
+
+
+function greeting() {
+    $names = "nov and khurt";
+    echo($names);
+}
+
+greeting(); 
+?>
